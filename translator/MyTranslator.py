c044e9a9be748787e19182ff861f2964fd57ac24
the-other-mariana <mariana.avalos.arce@gmail.com>
2023-06-13 14:02:52 -0600

    update: first commit
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

---
+++ translator/MyTranslator.py
@@ -0,0 +1,21 @@
+from translate import Translator
+from langdetect import detect
+
+class MyTranslator:
+    def __init__(self):
+        self.text = ''
+        # destiny language
+        self.to_lang = 'en'
+        self.from_lang = ''
+        self.translator = None
+    
+    def detect_language(self, text):
+        detected_lang = detect(text)
+        self.from_lang = detected_lang
+
+    def translate(self, text):
+        self.translator = Translator(to_lang=self.to_lang, from_lang=self.from_lang)
+        self.text = text
+        translation = translator.translate(text)
+        return translation
+         
