914f70b11553f6ee7749aa58cc187df95327a0c3
the-other-mariana <mariana.avalos.arce@gmail.com>
2023-06-30 12:50:32 -0600

    update: attention computation
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- text-summarizing/AbstractionSummary.py
+++ text-summarizing/AbstractionSummary.py
@@ -7,6 +7,48 @@ decoder_maxlen = 75
 BUFFER_SIZE = 20000
 BATCH_SIZE = 64
 
+def get_angles(position, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+    return position * angle_rates
+
+# positional encoding ensures the attention model preserves sequence in words, like a RNN
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(
+        np.arange(position)[:, np.newaxis],
+        np.arange(d_model)[np.newaxis, :],
+        d_model
+    )
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    
+    # apply cos to odd indices in the array; 2i + 1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    req[:, tf.newaxis, tf.newaxis, :]
+
+# look ahead mask is responsible for ignoring the words that occur after a given current word
+# in the target sequence
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+# basis for attention computation
+def scaled_dot_product_attention(q, k, v, mask):
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(attention_weights, v)
+    return output, attention_weights
+
 def main():
     url = 'dataset/inshorts-clean-data.csv'
     news = pd.read_csv(url, header=0)
@@ -30,6 +72,8 @@ def main():
     document_tokenizer.fit_on_texts(document)
     summary_tokenizer.fit_on_texts(summary)
 
+    # this maps an index to each word and thus makes a list of words appear as an integer vector
+    # word1 = 0 word2 = 1 and so on
     inputs = document_tokenizer.texts_to_sequences(document)
     targets = summary_tokenizer.texts_to_sequences(summary)
 
